2c113f098bb12d92415af0dec15bb944e093aa0f
NEVO <albertoborgato9@gmail.com>
2025-03-29 10:41:13 +0100

    Update Ch1.sql

--- Ch1.sql
+++ Ch1.sql
@@ -1,15 +1,3 @@
-
-CHAPTER 1
-What Is SQLite?
-Self-Contained, No Server Required
-Single File Database
-Zero Configuration
-Embedded Device Support
-Unique Features
-Compatible License
-Highly Reliable
-
-CHAPTER 2
-Uses of SQLite
-
-.select
+The Definitive Guide to SQLite
+Chapter 1
+Introducing SQLite
