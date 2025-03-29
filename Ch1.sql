b96e424f5a8208cfb417c693ac7f67642847e9e4
NEVO <albertoborgato9@gmail.com>
2025-03-29 10:43:09 +0100

    Update Ch1.sql

--- Ch1.sql
+++ Ch1.sql
@@ -1,3 +1,13 @@
 The Definitive Guide to SQLite
 Chapter 1
 Introducing SQLite
+An Embedded Database
+A Developer’s Database
+An Administrator’s Database
+SQLite History
+Who Uses SQLite
+Architecture
+The Compiler
+The Virtual Machine
+The Back End
+
