9f185b1ad2c69a815508716abeb5e929fe32dc5d
NEVO <albertoborgato9@gmail.com>
2025-03-29 09:59:32 +0100

    Create Ch1.sql

---
+++ Ch1.sql
@@ -0,0 +1,13 @@
+
+CHAPTER 1
+What Is SQLite?
+Self-Contained, No Server Required
+Single File Database
+Zero Configuration
+Embedded Device Support
+Unique Features
+Compatible License
+Highly Reliable
+
+CHAPTER 2
+Uses of SQLite
